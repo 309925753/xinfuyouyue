42715be12f51dcb2b7dabf920ab8ffb7c878089c
hey@quyangapp.com <he123456>
2020-04-29 19:25:16 +0800

    BUG

---
+++ skWeiChatBaidu/src/main/java/com/xfyyim/cn/bean/event/EventNotifyUpdate.java
@@ -0,0 +1,9 @@
+package com.xfyyim.cn.bean.event;
+
+public class EventNotifyUpdate {
+    public final String MessageData;
+
+    public EventNotifyUpdate(String MessageData) {
+        this.MessageData = MessageData;
+    }
+}
---
+++ skWeiChatBaidu/src/main/java/com/xfyyim/cn/bean/event/EventOnlieChat.java
@@ -0,0 +1,9 @@
+package com.xfyyim.cn.bean.event;
+
+public class EventOnlieChat {
+    public final String MessageData;
+
+    public EventOnlieChat(String MessageData) {
+        this.MessageData = MessageData;
+    }
+}
