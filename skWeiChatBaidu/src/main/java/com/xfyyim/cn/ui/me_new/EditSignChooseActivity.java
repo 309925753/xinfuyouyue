522d197bebb57c55679ed435734304a112507683
309925753 <cheng3099@126.com>
2020-05-08 15:52:00 +0800

    修复bug

--- skWeiChatBaidu/src/main/java/com/xfyyim/cn/ui/me_new/EditSignChooseActivity.java
+++ skWeiChatBaidu/src/main/java/com/xfyyim/cn/ui/me_new/EditSignChooseActivity.java
@@ -170,6 +170,13 @@ text=list.get(lastClickPosition);
                                 if (textContent != null && !TextUtils.isEmpty(textContent)) {
                                     if (!list.contains(textContent)){
                                         list.add(0,textContent);
+                                        lastClickPosition=0;
+                                    }else {
+                                        for (int i=0;i<list.size();i++){
+                                            if (list.get(i).equals(textContent)){
+                                                lastClickPosition=i;
+                                            }
+                                        }
                                     }
                                 }
 
@@ -209,6 +216,9 @@ text=list.get(lastClickPosition);
                 }
             }
         }
+
+
+
         return mlist;
     }
     private int lastClickPosition = -1;
